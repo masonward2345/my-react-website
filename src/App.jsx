5ea59a074c25248d7bfa5c181992416afc30c6de
Mason Ward <masonward2345@gmail.com>
2025-07-13 15:36:15 +0100

    Inital commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="App">
+        <header>
+          <h1>Hello</h1>
+        </header>
+
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Games</li>
+          </ul>
+        </nav>
+
+        <body>
+          <h3>This is my React App</h3>
+        </body>
+      </div>
+    </>
+  )
+}
+
+export default App
